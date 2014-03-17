a248851b1ce6dd26f839bb3296a6afe5bc19873f
Stephen Wright <ste.c.wr@gmail.com>
2014-03-17 16:12:59 +0000

    small commit to remove commenting from static pages

--- page.php
+++ page.php
@@ -11,7 +11,6 @@
 <div class="entry-links"><?php wp_link_pages(); ?></div>
 </section>
 </article>
-<?php if ( ! post_password_required() ) comments_template( '', true ); ?>
 <?php endwhile; endif; ?>
 </section>
 <?php get_sidebar(); ?>
